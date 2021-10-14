bda6add97d48b6ee651caf475b04f534bd710c35
Koval Yaroslav <koval@q-writer.com>
2021-10-14 21:03:05 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+
+class status(Resource):
+    def get(self) -> None:
+        return jsonify({'run': True})
+
+      
+class Random(Resource):
+    def get(self) -> None:
+        import random
+        json_data = {"random": random.uniform(0.00000001, 0.001)}
+        
+        return jsonify(json_data)
+
+
+api.add_resource(status, '/')
+api.add_resource(Random, '/random')
+
+if __name__ == '__main__':
+    app.run()
