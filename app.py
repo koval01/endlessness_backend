af8d09f16a665e6637423dbf6c8b6af7ef13657f
Koval Yaroslav <koval@q-writer.com>
2021-10-14 21:09:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
+import instaloader
+
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
