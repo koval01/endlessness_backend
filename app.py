08785fd0d4c1a78c8b136d816ce2197de2d12cc9
yaroslav.koval <ksvarog2004@gmail.com>
2021-10-17 18:34:06 +0300

    update

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import logging
 
-from flask import Flask, jsonify, g, Response
+from flask import Flask, jsonify, g, Response, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
 from instaloader import Instaloader
 from utils import PostProcess
 
+from requests import get
 from time import time
+
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,11 +19,29 @@ CORS(app)
 instagram_load = Instaloader()
 instagram_load.login("develop_account_kdv", "tiwjyg-9renva-jadwaV")
 
+enc_key = Fernet.generate_key()
+
 
 class Status(Resource):
     @staticmethod
     def get() -> Response:
         return jsonify({'run': True})
+
+
+class Image(Resource):
+    @staticmethod
+    def get() -> Response:
+        url = request.args["l"]
+        salt_link = Fernet(enc_key)
+        link_get = eval(salt_link.decrypt(str.encode(str(url))).decode('utf-8'))
+
+        if (link_get["time"] + 60) > time():
+            r = get(
+                link_get["url"], stream=True,
+                headers={'user-agent': request.headers.get('user-agent')}
+            )
+            return Response(r.iter_content(chunk_size=10 * 1024),
+                            content_type=r.headers['Content-Type'])
 
 
 class Random(Resource):
@@ -33,7 +54,7 @@ class Random(Resource):
             feed = instagram_load.get_explore_posts()
             for post in feed:
                 posts.append({
-                    "img_link": post.url,
+                    "img_link": PostProcess.encoder({"url": post.url, "time": time()}, enc_key),
                     "likes_count": PostProcess.number_formatter(int(post.likes)),
                     "shortcode": post.shortcode,
                     "caption": post.caption,
@@ -53,6 +74,7 @@ class Random(Resource):
 
 api.add_resource(Status, '/')
 api.add_resource(Random, '/random')
+api.add_resource(Image, '/image')
 
 if __name__ == '__main__':
     app.run()
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from cryptography.fernet import Fernet
+
+
 class PostProcess:
     @staticmethod
     def number_formatter(num: int) -> str:
@@ -9,3 +12,9 @@ class PostProcess:
             num /= 1000.0
 
         return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
+
+    @staticmethod
+    def encoder(link: dict, enc_key: bytes) -> str:
+        salt_link = Fernet(enc_key)
+        data_link = str.encode(str(link))
+        return salt_link.encrypt(data_link).decode("utf-8")
