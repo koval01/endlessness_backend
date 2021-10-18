50daf40567fbc344a1543996207d29b76ecd1970
yaroslav.koval <ksvarog2004@gmail.com>
2021-10-18 19:37:07 +0300

    update

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import logging
+import instaloader
 
 from flask import Flask, jsonify, g, Response, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
-
-from instaloader import Instaloader
 from utils import PostProcess
 
 from requests import get
@@ -16,8 +15,8 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-instagram_load = Instaloader()
-instagram_load.login("develop_account_kdv", "tiwjyg-9renva-jadwaV")
+instagram_load = instaloader.Instaloader()
+feed = instaloader.Hashtag.from_name(instagram_load.context, "stockings").get_posts()
 
 enc_key = b'r73QFT58DEGZIpGLUHW319V_brwT1pqMVcKa7cNDj_A='
 
@@ -31,42 +30,49 @@ class Status(Resource):
 class Image(Resource):
     @staticmethod
     def get() -> Response:
-        url = request.args["l"]
-        salt_link = Fernet(enc_key)
-        link_get = eval(salt_link.decrypt(str.encode(str(url))).decode('utf-8'))
+        try:
+            url = request.args["l"]
+            salt_link = Fernet(enc_key)
+            link_get = eval(salt_link.decrypt(str.encode(str(url))).decode('utf-8'))
 
-        if (link_get["time"] + 60) > time():
-            r = get(
-                link_get["url"], stream=True,
-                headers={'user-agent': request.headers.get('user-agent')}
-            )
-            return Response(r.iter_content(chunk_size=10 * 1024),
-                            content_type=r.headers['Content-Type'])
+            if (link_get["time"] + 60) > time():
+                r = get(
+                    link_get["url"], stream=True,
+                    headers={'user-agent': request.headers.get('user-agent')}
+                )
+                return Response(r.iter_content(chunk_size=10 * 1024),
+                                content_type=r.headers['Content-Type'])
+        except Exception as e:
+            logging.error("Error get image by enc key, details - %s" % e)
+            return jsonify({"success": False})
 
 
 class Random(Resource):
     @staticmethod
     def get() -> Response:
-        g.start = time()
-        posts = []
+        try:
+            g.start = time()
+            posts = []
 
-        feed = instagram_load.get_explore_posts()
-        for post in feed:
-            posts.append({
-                "img_link": PostProcess.encoder({"url": post.url, "time": time()}, enc_key),
-                "likes_count": PostProcess.number_formatter(int(post.likes)),
-                "shortcode": post.shortcode,
-                "caption": PostProcess.clean_caption(post.caption),
+            for post in feed:
+                posts.append({
+                    "img_link": PostProcess.encoder({"url": post.url, "time": time()}, enc_key),
+                    "likes_count": PostProcess.number_formatter(int(post.likes)),
+                    "shortcode": post.shortcode,
+                    "caption": PostProcess.clean_caption(post.caption),
+                })
+                if len(posts) >= 9:
+                    break
+
+            return jsonify({
+                "success": len(posts) != 0,
+                "time": round(time() - g.start, 3),
+                "posts": posts,
+                "count": len(posts),
             })
-            if len(posts) >= 9:
-                break
-
-        return jsonify({
-            "success": len(posts) != 0,
-            "time": round(time() - g.start, 3),
-            "posts": posts,
-            "count": len(posts),
-        })
+        except Exception as e:
+            logging.error("Error get random posts, details - %s" % e)
+            return jsonify({"success": False, "time": round(time() - g.start, 3)})
 
 
 api.add_resource(Status, '/')
